746a0e3a1e38aeae64b7c69e163ab468c58aca6b
Lanh Hoang <lanhhoang@users.noreply.github.com>
2019-12-21 22:39:52 +0700

    Add Supplier model

---
+++ db/migrate/20191221153900_create_suppliers.rb
@@ -0,0 +1,12 @@
+class CreateSuppliers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :suppliers do |t|
+      t.string :name, null: false
+      t.string :url, null: false
+
+      t.timestamps
+    end
+
+    add_index :suppliers, :name, unique: true
+  end
+end
