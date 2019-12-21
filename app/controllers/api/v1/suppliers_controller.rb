5c870acb9f2452663cf72a6c3040436c0f2a0382
Lanh Hoang <lanhhoang@users.noreply.github.com>
2019-12-21 23:53:11 +0700

    Modify search method of SuppliersController

--- app/controllers/api/v1/suppliers_controller.rb
+++ app/controllers/api/v1/suppliers_controller.rb
@@ -2,7 +2,29 @@ module Api
   module V1
     class SuppliersController < ::ApplicationController
       def search
-        render json: {}
+        service = SearchService.new(params: search_params)
+
+        if service.call
+          render json: service.hotels,
+                 status: :ok
+        else
+          render json: {
+            _error: service.errors.full_messages.to_sentence,
+            errors: service.errors.full_messages
+          }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def search_params
+        params.permit(
+          :checkin,
+          :checkout,
+          :destination,
+          :guests,
+          :suppliers
+        )
       end
     end
   end
