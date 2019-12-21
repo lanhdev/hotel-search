228cecf424f1a5e907ca2aaff0ab61123afea475
Lanh Hoang <lanhhoang@users.noreply.github.com>
2019-12-22 00:36:55 +0700

    Modify search method

--- app/controllers/api/v1/suppliers_controller.rb
+++ app/controllers/api/v1/suppliers_controller.rb
@@ -2,17 +2,10 @@ module Api
   module V1
     class SuppliersController < ::ApplicationController
       def search
-        service = SearchService.new(params: search_params)
+        service = Suppliers::SearchService.new(params: search_params).tap { |f| f.call }
 
-        if service.call
-          render json: service.hotels,
-                 status: :ok
-        else
-          render json: {
-            _error: service.errors.full_messages.to_sentence,
-            errors: service.errors.full_messages
-          }, status: :unprocessable_entity
-        end
+        render json: service.hotels,
+               status: :ok
       end
 
       private
