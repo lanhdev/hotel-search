90bb8750f8c430b60ca88bf25a1c5420e4d0823b
Lanh Hoang <lanhhoang@users.noreply.github.com>
2019-12-22 22:31:36 +0700

    Format response

--- app/services/suppliers/search_service.rb
+++ app/services/suppliers/search_service.rb
@@ -18,11 +18,12 @@ module Suppliers
 
     def fetch_from_cache
       @hotels = Rails.cache.fetch(cache_key, expires_in: CACHE_EXPIRES_IN) do
-        fetch_cheapest_hotel
+        hotels = fetch_cheapest_hotels
+        hotels.map { |hotel| [attributes, hotel].transpose.to_h }
       end
     end
 
-    def fetch_cheapest_hotel
+    def fetch_cheapest_hotels
       suppliers.map do |supplier|
         uri = URI.parse(supplier.url)
         response = Net::HTTP.get_response(uri)
@@ -32,6 +33,7 @@ module Suppliers
 
     def suppliers
       suppliers_param = params.dig(:suppliers).split(',')
+
       @suppliers ||=
         if suppliers_param.empty?
           Supplier.all
@@ -42,6 +44,10 @@ module Suppliers
 
     def cache_key
       params.values.join('_')
+    end
+
+    def attributes
+      %w(id price supplier)
     end
   end
 end
